3dc4ba4dadccc59d0d4f9c1fb6f68f146e7318c8
Lucas <lucas3213.lb@aluno.fapce.edu.br>
2020-12-01 18:18:31 -0300

    SmartGarden 0.1

---
+++ irrigacao.js
@@ -0,0 +1,55 @@
+
+        function desfavoritar0() {
+          document.getElementById("favorit0").src = "Imgs/off.png";
+        }
+        function favoritar0() {
+          document.getElementById("favorit0").src = "Imgs/on.png";
+        }
+
+// **********************************************************
+        function desfavoritar0() {
+            document.getElementById("favorit0").src = "Imgs/off.png";
+          }
+          function favoritar0() {
+            document.getElementById("favorit0").src = "Imgs/on.png";
+          }
+
+// **********************************************************
+          function desfavoritar1() {
+            document.getElementById("favorit1").src = "Imgs/off.png";
+          }
+          function favoritar1() {
+            document.getElementById("favorit1").src = "Imgs/on.png";
+          }
+
+// **********************************************************
+          function desfavoritar2() {
+            document.getElementById("favorit2").src = "Imgs/off.png";
+          }
+          function favoritar2() {
+            document.getElementById("favorit2").src = "Imgs/on.png";
+          }
+
+// **********************************************************
+          function desfavoritar3() {
+            document.getElementById("favorit3").src = "Imgs/off.png";
+          }
+          function favoritar3() {
+            document.getElementById("favorit3").src = "Imgs/on.png";
+          }
+
+// **********************************************************
+          function desfavoritar4() {
+            document.getElementById("favorit4").src = "Imgs/off.png";
+          }
+          function favoritar4() {
+            document.getElementById("favorit4").src = "Imgs/on.png";
+          }
+// **********************************************************
+          function desfavoritar5() {
+            document.getElementById("favorit5").src = "Imgs/off.png";
+          }
+          function favoritar5() {
+            document.getElementById("favorit5").src = "Imgs/on.png";
+          }
+    
